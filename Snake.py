08f1e228be25e8cb612b1e5ac9d6af1af69fbb94
aleonevenerius <alexandreleonevenerius@gmail.com>
2026-08-30 11:34:02 -0300

    Scenery fixed

--- Snake.py
+++ Snake.py
@@ -3,12 +3,12 @@ import time # Importing the Time library
 # The game's scenery
 scenery = """
 #################
-#		#
-#       	#
-#		#
-#		#
-#		#
-#	        #
+#               #
+#               #
+#               #
+#               #
+#               #
+#               #
 #################
 """
 print(scenery)
